8c9eb1a2a3e9687e67de68d24afaa56c20ca3240
rayyan99 <36453630+rayyan99@users.noreply.github.com>
2026-05-28 21:27:34 +0700

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
